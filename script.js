49a9e4b6e13fdca65adbc6f7cb5ead014b36c4cc
dm-hussain <message2hussain@gmail.com>
2024-09-01 20:26:31 +0530

    add how to play instruction popup feature on screen

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ const body = document.querySelector('body');
 const player = document.querySelector('#player');
 let currentPositionY = parseInt(window.getComputedStyle(player).bottom);
 let currentPositionX = parseInt(window.getComputedStyle(player).left);
+let instruction= document.querySelector('h5')
+
+
+window.addEventListener('load', ()=> {
+ 
+instruction.classList.add('load')
+instruction.classList.remove('unload')
+
+
+setTimeout(() => {
+    
+  instruction.classList.add('unload')
+  instruction.classList.remove('load')
+ 
+}, 4000);
+
+
+})
 
 body.addEventListener('keydown', (e) => {
   playMove(e.key);
+ 
+  instruction.classList.add('unload')
+  instruction.classList.remove('load')
 });
 
 function playMove(position) {
@@ -22,3 +43,5 @@ function playMove(position) {
     player.style.left = currentPositionX + 'px';
   }
 }
+
+ 
